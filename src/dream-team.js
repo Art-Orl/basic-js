8bb94deaf583be33158f9817978b03cde2daa4b1
Art-Orl <orlyuta95@mail.ru>
2021-03-22 01:30:57 +0300

    feat: dream team

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,24 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+// let arr = null;
+module.exports = function createDreamTeam(arr) {
+  let arr1 = [];
+  let arr2 = [];
+  let result;
+  if (arr == null) {
+    return false;
+  } else {
+    for (let i = 0; i < arr.length; i++) {
+      if (typeof arr[i] == "string")
+        arr1.push(arr[i].replace(/\s+/g, "").slice(0, 1));
+    }
+  }
+  let r = arr1.join("").toUpperCase();
+  arr2 = r.split("").sort();
+  result = arr2.join("");
+
+  return result;
 };
+
+// createDreamTeam(arr);
+// console.log(arr2.join(""));
