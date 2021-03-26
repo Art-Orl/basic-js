031c6a72a7154900e0b60635c80ef52fcff36572
Art-Orl <orlyuta95@mail.ru>
2021-03-26 12:04:09 +0300

    feat: what season

--- src/what-season.js
+++ src/what-season.js
@@ -1,16 +1,19 @@
 const CustomError = require("../extensions/custom-error");
 // date = new Date(2011, 6, 15)
 module.exports = function getSeason(date) {
-  if (date == null) return 'Unable to determine the time of year!'
+  if (date == null) { return 'Unable to determine the time of year!' };
+  if (Object.prototype.toString.call(date) !== "[object Date]")
+    throw new Error("Error");
+
   let m = date.getMonth();
   if (m == 0 || m == 1 || m == 11) {
-    return 'winter'
+    return 'winter';
   } else if (m == 2 || m == 3 || m == 4) {
-    return "spring"
+    return "spring";
   } else if (m == 5 || m == 6 || m == 7) {
-    return "summer"
+    return "summer";
   } else
-    return "autumn"
+    return "autumn";
 }
 
 
