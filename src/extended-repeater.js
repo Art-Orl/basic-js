9cdc765d0a1f846e6bd8ef89eb79a33b663b26fb
Art-Orl <orlyuta95@mail.ru>
2021-03-26 13:45:33 +0300

    feat: extended repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,43 @@
-const CustomError = require("../extensions/custom-error");
+// const CustomError = require("../extensions/custom-error");
+str = "STRING";
+// options = {
+//   repeatTimes: 3,
+//   separator: '**',
+//   addition: 'PLUS',
+//   additionRepeatTimes: 3,
+//   additionSeparator: '00'
+// }
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+// console.log(options.additionSeparator.length)
+module.exports = function repeater(str, options) {
+  if (!options.repeatTimes) {
+    options.repeatTimes = 1;
+  }
+  if (typeof options.addition === "undefined") {
+    options.addition = "";
+  }
+  if (!options.additionRepeatTimes) {
+    options.additionRepeatTimes = 1;
+  }
+  if (!options.additionSeparator) {
+    options.additionSeparator = "|";
+  }
+  if (!options.separator) {
+    options.separator = "+";
+  }
+  let result = "";
+  let y = ""
+  for (let j = 1; j < options.additionRepeatTimes + 1; j++) {
+    y += options.addition + options.additionSeparator;
+
+  }
+  let u = y.slice(0, -options.additionSeparator.length)
+  for (let i = 1; i < options.repeatTimes + 1; i++) {
+    result += options.separator + str + u;
+  }
+  let result1 = result.slice(options.separator.length)
+  return result1;
 };
-  
+
+// let x = repeater(str, options);
+// console.log(x);
