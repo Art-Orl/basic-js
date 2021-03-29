17faff900984f789c083363836a1201c3243ee5d
Art-Orl <orlyuta95@mail.ru>
2021-03-30 02:20:04 +0300

    feat: vigenere cipher

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,16 +1,46 @@
 const CustomError = require("../extensions/custom-error");
 
 class VigenereCipheringMachine {
+  constructor(direct = true) {
+    this.direct = direct;
+  }
   encrypt(message, key) {
-    if (!message || !key) {
-      throw new Error();
+    let str = [];
+    let code = 0;
+    let m = message.toUpperCase();
+    let k = key.toUpperCase();
+
+    for (let i = 0, j = 0; i < m.length; i++) {
+      if (m.charCodeAt(i) < 65 || m.charCodeAt(i) > 90) str[i] = m[i];
+      else {
+        code = (m.charCodeAt(i) + k.charCodeAt(j)) % 26;
+        str[i] = String.fromCharCode(code + 65);
+        if (j + 1 >= k.length) j = 0;
+        else j++;
+      }
     }
+    if (this.direct) return str.join("");
+    else return str.reverse().join("");
   }
 
   decrypt(message, key) {
-    if (!message || !key) {
-      throw new Error();
+    let str = [];
+    let code = 0;
+    let m = message.toUpperCase();
+    let k = key.toUpperCase();
+
+    for (let i = 0, j = 0; i < m.length; i++) {
+      if (m.charCodeAt(i) < 65 || m.charCodeAt(i) > 90) str[i] = m[i];
+      else {
+        code = (m.charCodeAt(i) + 26 - k.charCodeAt(j)) % 26;
+        str[i] = String.fromCharCode(code + 65);
+        if (j + 1 >= k.length) j = 0;
+        else j++;
+      }
     }
+
+    if (this.direct) return str.join("");
+    else return str.reverse().join("");
   }
 }
 
